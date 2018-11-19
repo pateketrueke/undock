9ce00d3b6405d57f31009d8d800e70c44ab6c669
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-11-18 21:17:26 -0600

    Run both commands in order; fix

--- attach.sh
+++ attach.sh
@@ -23,5 +23,5 @@ GITCONFIG="-v $HOME/.gitconfig:/home/dev/.gitconfig"
 SSHDIR="-v $HOME/.ssh:/home/dev/.ssh"
 HOMEDIR="-v $PWD:/usr/src/dev"
 
-docker build --target $BUILD_TARGET -t $PROJECT_NAME -f $DOCKER_FILE .
+docker build --target $BUILD_TARGET -t $PROJECT_NAME -f $DOCKER_FILE . && \
 docker run -it --privileged $SOCKET $GITCONFIG $SSHDIR $HOMEDIR $PROJECT_NAME $EXEC
